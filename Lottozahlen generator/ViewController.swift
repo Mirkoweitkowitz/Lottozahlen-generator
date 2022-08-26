ea92fd6d82461ce2a3318ffde70994bbc6fe7ea5
Mirkoweitkowitz <99251369+Mirkoweitkowitz@users.noreply.github.com>
2022-08-26 10:26:54 +0200

    Initial Commit

---
+++ Lottozahlen generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lottozahlen generator
+//
+//  Created by Mirko Weitkowitz on 26.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
